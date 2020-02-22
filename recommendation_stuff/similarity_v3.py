8d4bdc02619a3f4d97589e3eb8d12dfbccc34c2a
Reetahan <rm10@illinois.edu>
2020-02-22 16:50:56 -0600

    more working version of similarilty algo :D

--- recommendation_stuff/similarity_v3.py
+++ recommendation_stuff/similarity_v3.py
@@ -1,7 +1,12 @@
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 import nltk
+import nltk.data
 import numpy as np
-from nltk.tokenize import word_tokenize
+import smart_open
+from nltk import tokenize
+import string
+from nltk import word_tokenize
+import gensim
 
 
 #complete
@@ -9,40 +14,68 @@ def main():
 	nltk.download('punkt')
 	model_pathname = "rec_alg.model"
 	data_path = "data_list"
-	classify_path = "classify_list.txt"
+	classify_student_path = "classify_list.txt"
+	classify_posting_path = "hehe.txt"
 	output_pathname = "classified.txt"
-	labels = [["machine", "learning"],["chatting"],["apple"]]
-
-	data = load_data(data_path)
-	#print(data)
+	
+	data = load_data(data_path, True)
+	print(data)
 	model = train_model(data)
 	save_model(model, model_pathname)
 
-	to_classify = load_data(classify_path)
-	#print(to_classify)
-	classify_results = classify(to_classify,model_pathname, labels)
+	students = load_data(classify_student_path, False)
+	postings = load_data(classify_posting_path, False)
+	classify_results = classify(students,model_pathname, postings)
 	save_results(classify_results, output_pathname)
+	print("Program complete")
+
+
+TABLE_TRANS = str.maketrans({key: ' ' for key in string.punctuation})
+TABLE_TRANS['.'] = ' . '
+TABLE_TRANS['?'] = ' . '
+TABLE_TRANS['!'] = ' . '
+TABLE_TRANS[':'] = ' . '
+TABLE_TRANS[';'] = ' . '
+TABLE_TRANS['('] = ' '
+TABLE_TRANS[')'] = ' '
+TABLE_TRANS['['] = ' '
+TABLE_TRANS[']'] = ' '
+TABLE_TRANS[','] = ' '
+TABLE_TRANS['{'] = ' '
+TABLE_TRANS['}'] = ' '
+
+
+def clean_text(s):
+    words = str(s).lower().translate(TABLE_TRANS).split()
+    return " ".join(words)
 
 
 
+#turn files into TaggedDocs
+def load_data(data_path, build_model):
+	if(build_model):
+		fp = open(data_path)
+		p = fp.read()
+		data = tokenize.sent_tokenize(p)
 
-
-
-#TO-DO
-def load_data(data_path):
-	if(data_path == "classify_list.txt"):
-		classif = [["machine", "learning"],["chatbots"]]
-		test_data = classif#[word_tokenize("machine learning".lower()),word_tokenize("chatbots".lower())]
-		return test_data
+		print(data)
+		return [TaggedDocument(words=clean_text(s).split(), tags=[i]) for i, s in enumerate(data)]
 	else:
-		data = ["I love machine learning. Its awesome.","I love coding in python", "I love building chatbots", "they chat amazingly well", "machine learning"]
-		tagged_data = [TaggedDocument(words=word_tokenize(_d.lower()), tags=[str(i)]) for i, _d in enumerate(data)]
-		return tagged_data
+		with open(data_path) as f:
+			res = []
+			for line in f:
+				cur_entr = []
+				words = line.split(',')
+				for w in words:
+					ms = w.split()
+					cur_entr.append(ms)
+				res.append(cur_entr)
+			return res
 
 #complete
 def train_model(data):
 	max_epochs = 100
-	vec_size = 20
+	vec_size = 50
 	alpha = 0.025
 	model = Doc2Vec(size=vec_size,
 	                alpha=alpha,
@@ -60,21 +93,34 @@ def train_model(data):
 	return model
 
 #To-do
-def classify(to_classify, model_pathname,labels):
+def classify(student_classify,model_pathname,posting_classify):
 	model= Doc2Vec.load(model_pathname)
 	ret_val = dict()
-	#to find the vector of a document which is not in training data
-	for term in to_classify[0]:
-		sim_scores = np.array([-100.1])
-		for choice in labels[0]:
-			#print(choice)
-			#print(term)
-			res = model.wv.similarity(term,choice)
-			sim_scores = np.append(sim_scores,res)
-		idx = np.argmax(sim_scores)
-		ret_val[term] = labels[idx]
+
+	for student in student_classify:
+		scores = np.array([-1.0])
+		scores = np.delete(scores,0)
+		for posting in posting_classify:
+			cur_score = 0.0
+			for attr in student:
+				term_score = 0.0
+				for post_attr in posting:
+					
+					term_score = term_score + model.wv.n_similarity(attr,post_attr)
+				cur_score = cur_score + term_score
+			scores = np.append(scores,cur_score)
+		top = posting_classify[np.argmax(scores)]
+		
+		temp = []
+		for entity in student:
+			val = tuple(entity)
+			temp.append(val)
+		tup_stud = tuple(temp)
+		ret_val[tup_stud] = top			
+		
 	return ret_val
 
+#done
 def save_results(classify_results,output_pathname):
 	with open(output_pathname, "w") as f:
 		for key in classify_results:
@@ -83,6 +129,7 @@ def save_results(classify_results,output_pathname):
 			f.write("\n")
 
 
+#done
 def save_model(model,model_file):
 	model.save(model_file)
 	print('Model saved in ' + model_file + '!')
